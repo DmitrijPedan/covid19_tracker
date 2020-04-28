d347c5b4968838a6d55e2d420b75058e96d886c3
DmitrijPedan <dmitrijpedan84@gmail.com>
2020-04-28 14:01:02 +0300

    add appbar to header

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 Информация о распостранении COVID-19 в реальном времени. 
 Источники: 
     Информация о COVID-19: https://covid19.mathdro.id/api;
+
+Проект доступен на Heroku: https://covid-inform.herokuapp.com/
+
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,29 +1,38 @@
 import React from 'react';
-import {Typography, Grid, Container, FormControlLabel, Switch} from '@material-ui/core';
-import image from '../../img/covid.png';
+import {AppBar, Toolbar, Grid, IconButton, Container, FormControlLabel, Switch, Typography} from '@material-ui/core';
+import {makeStyles} from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
 import styles from './Header.module.scss';
+
+const useStyles = makeStyles((theme) => ({
+    root: {flexGrow: 1},
+    menuButton: {marginRight: theme.spacing(2)},
+    title: {flexGrow: 1},
+  }));
 
 const Header = ({changeTheme}) => {
 
+    const classes = useStyles();
+      
     return (
         <header>
-            <Container className = {styles.container}>
-                <Grid container spacing = {1} justify = "center" direction = "column" alignItems = "center"> 
-                    <FormControlLabel
-                        control={
-                        <Switch onChange={changeTheme} color="success"/>
-                        }
-                        label="Темная тема"
-                    />
-                    <img src={image} alt="COVID-19"/> 
-                    <Typography variant="h2" className = "title" color = "textPrimary">
-                        COVID-19
-                    </Typography>
-                    <Typography variant = "subtitle1" className = "subtitle" color = "textSecondary">
-                        Официальная статистика
-                    </Typography> 
-                </Grid>
-            </Container>
+        <AppBar position="sticky" color = "default">
+            <Toolbar>
+                <IconButton edge="start" className={classes.menuButton} aria-label="menu">
+                    <MenuIcon />
+                </IconButton>
+                <Typography variant="h6" className={classes.title}>Меню</Typography>
+                <FormControlLabel control={<Switch onChange={changeTheme} color="success"/>} label="Тема" />
+            </Toolbar>
+        </AppBar>
+        <Container className = {styles.container}>
+            <Grid container spacing = {1} justify = "center" direction = "column" alignItems = "center"> 
+                <Typography variant="h2" className = "title" color = "textPrimary">COVID-19</Typography>
+                <Typography variant = "subtitle1" className = "subtitle" color = "textSecondary">
+                    Официальная статистика
+                </Typography> 
+            </Grid>
+        </Container>
         </header>
     )
 }
