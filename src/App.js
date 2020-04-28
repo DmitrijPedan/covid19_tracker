8e43ccfe8b098288dc97aab0afdde13fa2679338
DmitrijPedan <dmitrijpedan84@gmail.com>
2020-04-28 13:09:51 +0300

    light dark theme

--- src/App.js
+++ src/App.js
@@ -1,40 +1,78 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useMemo} from 'react';
 import {fetchCovidData} from './services/fetchApi';
 import {Loader, Header, InfoCard, CountrySelect, Chart, Table} from './components';
+import {createMuiTheme, ThemeProvider} from '@material-ui/core/styles';
+import {FormControlLabel, Switch} from '@material-ui/core';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 const App = () => {
-
-    document.title = 'COVID-19 Инфо';
-
-    const [covidData, setCovidData] = useState(null);
-    const [selectedCountry, setSelectedCountry] = useState('');
+    
+    const [state, setState] = useState({
+        themePalette: 'light',
+        covidData: null,
+        selectedCountry: '',
+    });
+   
+        
+    const theme = useMemo(() => createMuiTheme({
+        palette: {
+            type:  state.themePalette,
+        },
+        })
+      );
 
     useEffect(() => {
         const fetchData = async () => {
             const fetchedData = await fetchCovidData();
             if (fetchedData) {
-                setCovidData(fetchedData);
+                setState({...state, covidData: fetchedData});
             };
         } 
         fetchData();
     },[]);
 
     const handleCountrySelect = async (selectedCountry) => {
-        setCovidData(await fetchCovidData(selectedCountry));
-        setSelectedCountry(selectedCountry);
+        setState({
+            ...state, 
+            covidData: await fetchCovidData(selectedCountry), 
+            selectedCountry
+        })
     }
 
-    if (!covidData) {
+    const changeTheme = () => {
+        if(state.themePalette === 'dark') {
+            setState({
+                ...state,
+                themePalette: 'light'
+            })
+        } else {
+            setState({
+                ...state,
+                themePalette: 'dark'
+            })
+        }
+        
+    }
+
+    if (!state.covidData) {
         return <Loader />
     } else {
         return (
-            <div>
+            <ThemeProvider theme={theme}>
+                     <FormControlLabel
+                        control={
+                        <Switch onChange={changeTheme} color="secondary"/>
+                        }
+                        label="Темная тема"
+                    />
+               
+                <CssBaseline />
                 <Header />
                 <CountrySelect handleCountrySelect = {handleCountrySelect}/>
-                <InfoCard data = {covidData.data}/>
-                <Chart countryData = {covidData.data} country = {selectedCountry}/>
+                <InfoCard data = {state.covidData.data}/>
+                <Chart countryData = {state.covidData.data} country = {state.selectedCountry}/>
                 <Table />
-            </div>
+            </ThemeProvider>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+document.title = 'COVID-19 Инфо';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
